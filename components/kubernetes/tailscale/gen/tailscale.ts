d0cbd5ab3dd8c87248f6286bbd5caa926e32cb64
liam <liamawhite@gmail.com>
2025-02-03 15:15:06 -0800

    tidy

--- components/kubernetes/tailscale/gen/tailscale.ts
+++ components/kubernetes/tailscale/gen/tailscale.ts
@@ -16,21 +16,3 @@ export async function tailscale() {
         ],
     })
 }
-
-// #!/bin/bash
-//
-// VERSION = 1.78.3
-// DIR = "$( cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd )"
-//
-// # Generate strongly typed code for working with the custom resources
-// rm - rf "${DIR}/crds"
-// mkdir - p "${DIR}/crds"
-// crd2pulumi--nodejsPath "$DIR/crds" --force \
-// "https://raw.githubusercontent.com/tailscale/tailscale/refs/tags/v${VERSION}/cmd/k8s-operator/deploy/crds/tailscale.com_connectors.yaml" \
-// "https://raw.githubusercontent.com/tailscale/tailscale/refs/tags/v${VERSION}/cmd/k8s-operator/deploy/crds/tailscale.com_dnsconfigs.yaml" \
-// "https://raw.githubusercontent.com/tailscale/tailscale/refs/tags/v${VERSION}/cmd/k8s-operator/deploy/crds/tailscale.com_proxyclasses.yaml" \
-// "https://raw.githubusercontent.com/tailscale/tailscale/refs/tags/v${VERSION}/cmd/k8s-operator/deploy/crds/tailscale.com_proxygroups.yaml" \
-// "https://raw.githubusercontent.com/tailscale/tailscale/refs/tags/v${VERSION}/cmd/k8s-operator/deploy/crds/tailscale.com_recorders.yaml" \
-//
-// rm "${DIR}/crds/tsconfig.json" "${DIR}/crds/README.md" "${DIR}/crds/package.json"
-//
