6ea5f624b8b394a57e9afa282283cc98bb7a22be
Liam White <liamawhite@gmail.com>
2025-08-02 18:17:27 -0700

    longhorn crds

--- .gen.ts
+++ .gen.ts
@@ -1,7 +1,8 @@
 import { certmanager } from './components/kubernetes/certmanager/gen/certmanager'
 import { gatewayapi } from './components/kubernetes/istio/gen/gatewayapi'
+import { longhorn } from './components/kubernetes/longhorn/gen/longhorn'
 import { metallb } from './components/kubernetes/metallb/gen/metallb'
 import { tailscale } from './components/kubernetes/tailscale/gen/tailscale'
 ;(async () => {
-    await Promise.all([gatewayapi(), certmanager(), metallb(), tailscale()])
+    await Promise.all([gatewayapi(), certmanager(), longhorn(), metallb(), tailscale()])
 })()
