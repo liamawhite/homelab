1b39875d07d16759338a8cd5986b37072de90107
Liam White <liamawhite@gmail.com>
2025-08-08 00:51:04 -0700

    version bumps

--- .versions.ts
+++ .versions.ts
@@ -4,14 +4,14 @@ import { execSync } from 'child_process'
 const currentCommitSha = execSync('git rev-parse HEAD', { encoding: 'utf8' }).trim()
 
 export const versions = {
-    certManager: '1.16.3',
+    certManager: '1.18.2',
     coredns: currentCommitSha,
     externalDns: '0.15.1',
     gatewayApi: '1.2.0',
     istio: '1.26.3',
     k8sGateway: '3.2.3', // https://github.com/k8s-gateway/k8s_gateway/pkgs/container/charts%2Fk8s-gateway/versions
     longhorn: '1.9.1',
-    metallb: '0.14.9',
+    metallb: '0.15.2',
     nodeExporter: '1.9.1',
     pihole: '2024.07.0',
     prometheus: '3.5.0',
